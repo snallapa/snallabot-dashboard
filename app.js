8164678828475c157c415446bdc375c7871a3956
Sahith Nallapareddy <sahith.reddy@gmail.com>
2023-10-11 02:48:41 -0400

    blaze login

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const firebaseapp = require("firebase/app");
 const firestore = require("firebase/firestore");
+const crypto = require("crypto");
+const buffer = require("buffer");
 
 const express = require("express");
 
@@ -440,6 +442,222 @@ app.post(
   },
 );
 
+const TWO_DIGIT_YEAR = "24";
+const YEAR = "2024";
+
+const VALID_ENTITLEMENTS = (a) => ({
+  xone: `MADDEN_${a}XONE`,
+  ps4: `MADDEN_${a}PS4`,
+  pc: `MADDEN_${a}PC`,
+  ps5: `MADDEN_${a}PS5`,
+  xbsx: `MADDEN_${a}XBSX`,
+  stadia: `MADDEN_${a}SDA`,
+});
+
+const ENTITLEMENT_TO_SYSTEM = (a) => ({
+  [`MADDEN_${a}XONE`]: "xone",
+  [`MADDEN_${a}PS4`]: "ps4",
+  [`MADDEN_${a}PC`]: "pc",
+  [`MADDEN_${a}PS5`]: "ps5",
+  [`MADDEN_${a}XBSX`]: "xbsx",
+  [`MADDEN_${a}SDA`]: "stadia",
+});
+
+const BLAZE_SERVICE = (a) => ({
+  xone: `madden-${a}-xone-gen4`,
+  ps4: `madden-${a}-ps4-gen4`,
+  pc: `madden-${a}-pc-gen5`,
+  ps5: `madden-${a}-ps5-gen5`,
+  xbsx: `madden-${a}-xbsx-gen5`,
+  stadia: `madden-${a}-stadia-gen5`,
+});
+const BLAZE_PRODUCT_NAME = (a) => ({
+  xone: `madden-${a}-xone-mca`,
+  ps4: `madden-${a}-ps4-mca`,
+  pc: `madden-${a}-pc-mca`,
+  ps5: `madden-${a}-ps5-mca`,
+  xbsx: `madden-${a}-xbsx-mca`,
+  stadia: `madden-${a}-stadia-mca`,
+});
+
+async function refreshToken(guild_id) {
+  const docSnap = await firestore.getDoc(
+    firestore.doc(db, "leagues", guild_id),
+  );
+  if (!docSnap.exists()) {
+    throw new Error(`No league found for ${guild_id}, export in MCA first`);
+  }
+  const league = docSnap.data();
+  const tokenInfo = league.madden_server;
+  const now = new Date();
+  if (tokenInfo.accessToken && now > tokenInfo.expiry) {
+    //refresh token
+    const res1 = await fetch(`https://accounts.ea.com/connect/token`, {
+      method: "POST",
+      headers: {
+        "Accept-Charset": "UTF-8",
+        "User-Agent":
+          "Dalvik/2.1.0 (Linux; U; Android 13; sdk_gphone_x86_64 Build/TE1A.220922.031)",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Accept-Encoding": "gzip",
+      },
+      body: `grant_type=refresh_token&client_id=MaddenCompanionApp19&client_secret=U02zL2khwlgFdODLWCyLPmccOc5EiPBBYcEwCqKDTWBrZj2Fwcx4b4Zd9rsA6lHLkIU4qlVvNh84olij&release_type=prod&refresh_token=${tokenInfo.refreshToken}}&authentication_source=317239&token_format=JWS`,
+    });
+    const res1Json = await res1.json();
+    const {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+      expires_in: expiresIn,
+    } = res1Json;
+    const expiry = new Date(new Date().getTime() + expiresIn * 1000);
+    await firestore.setDoc(
+      firestore.doc(db, "leagues", guild_id),
+      {
+        madden_server: {
+          accessToken,
+          refreshToken,
+          expiry,
+        },
+      },
+      { merge: true },
+    );
+  }
+}
+
+const BLAZE_SESSION_EXPIRY = 1140;
+
+async function getBlazeSession(guild_id) {
+  const docSnap = await firestore.getDoc(
+    firestore.doc(db, "leagues", guild_id),
+  );
+  if (!docSnap.exists()) {
+    throw new Error(`No league found for ${guild_id}, export in MCA first`);
+  }
+
+  const league = docSnap.data();
+  const tokenInfo = league.madden_server;
+  const now = new Date();
+  if (tokenInfo.sessionKey && now > tokenInfo.blazeExpiry) {
+    const res1 = await fetch(
+      `https://wal2.tools.gos.bio-iad.ea.com/wal/authentication/login`,
+      {
+        method: "POST",
+        headers: {
+          "Accept-Charset": "UTF-8",
+          Accept: "application/json",
+          "X-BLAZE-ID": tokenInfo.blazeId,
+          "X-BLAZE-VOID-RESP": "XML",
+          "X-Application-Key": "MADDEN-MCA",
+          "Content-Type": "application/json",
+          "User-Agent":
+            "Dalvik/2.1.0 (Linux; U; Android 13; sdk_gphone_x86_64 Build/TE1A.220922.031)",
+        },
+        body: JSON.stringify({
+          accessToken: tokenInfo.accessToken,
+          productName: tokenInfo.blazeProductName,
+        }),
+      },
+    );
+    const res1Json = await res1.json();
+    const {
+      sessionKey,
+      userLoginInfo: {
+        personaDetails: { personaId: blazeId },
+      },
+    } = res1Json;
+    tokenInfo.sessionKey = sessionKey;
+    const blazeExpiry = new Date(
+      new Date().getTime() + BLAZE_SESSION_EXPIRY * 1000,
+    );
+    tokenInfo.blazeSessionExpiry = blazeExpiry;
+    tokenInfo.blazeId = blazeId;
+    await firestore.setDoc(
+      firestore.doc(db, "leagues", discord),
+      {
+        madden_server: tokenInfo,
+      },
+      { merge: true },
+    );
+  }
+}
+
+function calculateMessageAuthData(blazeId, requestId) {
+  const rand4bytes = crypto.randomBytes(4);
+  const requestData = JSON.stringify({
+    staticData: "05e6a7ead5584ab4",
+    requestId: requestId,
+    blazeId: blazeId,
+  });
+  const staticBytes = buffer.Buffer.from(
+    "634203362017bf72f70ba900c0aa4e6b",
+    "hex",
+  );
+
+  const xorHash = crypto
+    .createHash("md5")
+    .update(rand4bytes)
+    .update(staticBytes)
+    .digest();
+  const requestBuffer = buffer.Buffer.from(requestData, "utf-8");
+  const scrambledBytes = requestBuffer.map((b, i) => b ^ xorHash[i % 16]);
+  const authDataBytes = buffer.Buffer.concat([rand4bytes, scrambledBytes]);
+  const staticAuthCode = buffer.Buffer.from(
+    "3a53413521464c3b6531326530705b70203a2900",
+    "hex",
+  );
+
+  const authCode = crypto
+    .createHash("md5")
+    .update(staticAuthCode)
+    .update(authDataBytes)
+    .digest("base64");
+  const authData = authDataBytes.toString("base64");
+  const authType = 17039361;
+  return { authData, authCode, authType };
+}
+
+async function makeBlazeRequest(guild_id, blazeRequest) {
+  const docSnap = await firestore.getDoc(
+    firestore.doc(db, "leagues", guild_id),
+  );
+  if (!docSnap.exists()) {
+    throw new Error(`No league found for ${guild_id}, export in MCA first`);
+  }
+
+  const league = docSnap.data();
+  const tokenInfo = league.madden_server;
+  const requestId = tokenInfo.blazeRequestId || 2;
+  const authData = calculateMessageAuthData(tokenInfo.blazeId, requestId);
+  blazeRequest.messageAuthData = authData;
+  const messageExpiration = Math.floor(new Date().getTime() / 1000);
+  blazeRequest.messageExpirationTime = messageExpiration;
+  blazeRequest.deviceId = "MCA4b35d75Vm-MCA";
+  blazeRequest.ipAddress = "127.0.0.1";
+
+  const res1 = await fetch(
+    `https://wal2.tools.gos.bio-iad.ea.com/wal/authentication/login`,
+    {
+      method: "POST",
+      headers: {
+        "Accept-Charset": "UTF-8",
+        Accept: "application/json",
+        "X-BLAZE-ID": tokenInfo.blazeId,
+        "X-BLAZE-VOID-RESP": "XML",
+        "X-Application-Key": "MADDEN-MCA",
+        "Content-Type": "application/json",
+        "User-Agent":
+          "Dalvik/2.1.0 (Linux; U; Android 13; sdk_gphone_x86_64 Build/TE1A.220922.031)",
+      },
+      body: JSON.stringify({
+        apiVersion: 2,
+        clientDevice: 3,
+        requestInfo: JSON.stringify(blazeRequest),
+      }),
+    },
+  );
+  return await res1.json();
+}
+
 app.post("/:discord/linkea", async (req, res, next) => {
   const {
     params: { discord },
@@ -497,6 +715,9 @@ app.post("/:discord/linkea", async (req, res, next) => {
       expires_in: expiresIn,
     } = res1Json;
     const expiry = new Date(new Date().getTime() + expiresIn * 1000);
+    const blazeId = BLAZE_SERVICE(YEAR)[consoleAbbr];
+    const blazeProductName = BLAZE_PRODUCT_NAME(YEAR)[consoleAbbr];
+
     await firestore.setDoc(
       firestore.doc(db, "leagues", discord),
       {
@@ -504,10 +725,33 @@ app.post("/:discord/linkea", async (req, res, next) => {
           accessToken,
           refreshToken,
           expiry,
+          blazeId,
+          blazeProductName,
         },
       },
       { merge: true },
     );
+    const consoleAbbr = ENTITLEMENT_TO_SYSTEM(TWO_DIGIT_YEAR)[gameConsole];
+    await getBlazeSession(discord);
+    const leagueResponse = await makeBlazeRequest(discord, {
+      commandName: "Mobile_GetMyLeagues",
+      componentId: 2060,
+      commandId: 801,
+      requestPayload: {},
+      componentName: "careermode",
+    });
+    console.log(leagueResponse);
+    const {
+      responseInfo: {
+        value: { leagues: maddenLeagues },
+      },
+    } = leagueResponse;
+    const slimmedLeagues = maddenLeagues.map((m) => ({
+      leagueId: m.leagueId,
+      leagueName: m.leagueName,
+      userTeamName: m.userTeamName,
+    }));
+    res.status(200).json(slimmedLeagues);
   } catch (e) {
     next(e);
   }
