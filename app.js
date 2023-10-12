ac5fcf36506035c7b2c411b0d58885ea16262565
Sahith Nallapareddy <sahith.reddy@gmail.com>
2023-10-12 00:57:26 -0400

    logs

--- app.js
+++ app.js
@@ -505,6 +505,7 @@ async function refreshToken(guild_id) {
       body: `grant_type=refresh_token&client_id=MaddenCompanionApp19&client_secret=U02zL2khwlgFdODLWCyLPmccOc5EiPBBYcEwCqKDTWBrZj2Fwcx4b4Zd9rsA6lHLkIU4qlVvNh84olij&release_type=prod&refresh_token=${tokenInfo.refreshToken}}&authentication_source=317239&token_format=JWS`,
     });
     const res1Json = await res1.json();
+    console.log(res1Json);
     const {
       access_token: accessToken,
       refresh_token: refreshToken,
